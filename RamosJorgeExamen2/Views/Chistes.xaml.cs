08520fe1f58c879164c0e9db2a56b217f5c3c85e
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2025-06-02 08:33:16 -0500

    implementacion de API

--- RamosJorgeExamen2/Views/Chistes.xaml.cs
+++ RamosJorgeExamen2/Views/Chistes.xaml.cs
@@ -1,9 +1,35 @@
+using System.Text.Json;
+
 namespace RamosJorgeExamen2.Views;
 
 public partial class Chistes : ContentPage
 {
-	public Chistes()
-	{
-		InitializeComponent();
-	}
+
+    public Chistes()
+    {
+        InitializeComponent();
+        CargarChiste();
+    }
+
+    private async void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        await CargarChiste();
+    }
+
+    private async Task CargarChiste()
+    {
+
+        var httpClient = new HttpClient();
+        var json = await httpClient.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+        var joke = JsonSerializer.Deserialize<Joke>(json);
+        Chiste.Text = $"{joke.setup}\n\n{joke.punchline}";
+
+    }
+
+    public class Joke
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
 }
+
